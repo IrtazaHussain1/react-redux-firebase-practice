abec76cc142f2d43c4cd9cf3bd28efad1f72ccf1
Muhammad Irtaza Hussain <38260167+IrtazaHussain1@users.noreply.github.com>
2020-02-16 19:20:27 +0500

    Update fbConfig.js

--- src/config/fbConfig.js
+++ src/config/fbConfig.js
@@ -4,13 +4,7 @@ import 'firebase/firestore'
 import 'firebase/auth'
 
 var firebaseConfig = {
-    apiKey: "AIzaSyAcb4IhbR7ej28YLe14i033NWu4-dABrsQ",
-    authDomain: "marioplan-c6c07.firebaseapp.com",
-    databaseURL: "https://marioplan-c6c07.firebaseio.com",
-    projectId: "marioplan-c6c07",
-    storageBucket: "marioplan-c6c07.appspot.com",
-    messagingSenderId: "398070425354",
-    appId: "1:398070425354:web:287ac4aba043be068c7cf3"
+    // replace with your config 
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
